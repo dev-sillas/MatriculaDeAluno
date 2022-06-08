0208585a4d4f79b5e0fa76399d026f795bdcca90
Sillas Torres <71530830+dev-sillas@users.noreply.github.com>
2022-06-07 22:59:53 -0300

    trocando o banco

--- projetoMVC/conexao/ConnectionFactory.java
+++ projetoMVC/conexao/ConnectionFactory.java
@@ -1,17 +1,17 @@
-package projetoMVC.conexao;
-
+package br.com.exemplo.ultil;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+
 import javax.swing.JOptionPane;
 
 public class ConnectionFactory {
 	public static Connection getConnection() throws Exception{
 		try {
-		Class.forName("com.mysql.jdbc.Driver");
+		Class.forName("com.mysql.cj.jdbc.Driver");
 		String login = "root";
 		String senha = "";
-		String url = "jdbc:mysql://localhost:3306/mvcdb";
+		String url = "jdbc:mysql://localhost:3306/mvc";
 		return DriverManager.getConnection(url,login,senha);
 		} catch(Exception e) {
 			throw new Exception(e.getMessage());
@@ -20,9 +20,10 @@ public class ConnectionFactory {
 	public static void main(String[] args){
 		try {
 		Connection conn = ConnectionFactory.getConnection();
-		JOptionPane.showMessageDialog(null, "DB CONECTADA");
+		JOptionPane.showMessageDialog(null, "amv CONECTADA");
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
 }
+
